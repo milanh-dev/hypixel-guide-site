fdc36541fd8a6cd4425e8951cc3600c257d763ec
milanhaaijer <milanhaaijer2004@gmail.com>
2024-07-05 16:40:38 +0200

    feat: added user seeders

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::factory()->create([
+            'name' => 'milan',
+            'email' => 'milan@example.com',
+            'password' => Hash::make('password')
+        ]);
+    }
+}
